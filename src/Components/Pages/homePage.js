c33e1a1adc7d38b46f99c97337410d6702d8ea98
anthony-mendola <anthonymendola@gmail.com>
2018-11-26 23:32:13 -0500

    working on nav bar

--- src/Components/Pages/homePage.js
+++ src/Components/Pages/homePage.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Homepage extends Component {
   render() {
     return (
- <div className="wrap">
+ <div className="container-fluid">
   This is my home page
  </div>
     );
