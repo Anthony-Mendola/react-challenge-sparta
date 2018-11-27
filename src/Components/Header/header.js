f417d03c032d32c3aa2c646804701eba18a45d8c
anthony-mendola <anthonymendola@gmail.com>
2018-11-26 21:40:25 -0500

    first commit

---
+++ src/Components/Header/header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+     <header>
+<div className="logo">
+LOGO</div>
+     <nav>
+       <ul>
+         <li>
+           <a href="#">Login</a>
+         </li>
+       </ul>
+     </nav>
+     </header>
+    );
+  }
+}
+
+export default Header;
